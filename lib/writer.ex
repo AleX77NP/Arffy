0574d72fef4b1780785216e85a7fe9b7502a0be4
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-08-30 19:15:33 +0200

    refactor write, head and data..

--- lib/writer.ex
+++ lib/writer.ex
@@ -1,8 +1,17 @@
 defmodule Arffy.Writer do
 
-  def write_data(path, data) do
+  def write_header(file) do
+    IO.puts(file, "@relation 'my-arffy-relation'\n\n")
+  end
+
+  def write_data(file, data) do
+    IO.puts(file,"@data\n#{data}")
+  end
+
+  def write_content(path, data) do
     with {:ok, file} <- File.open(path, [:append]) do
-      IO.puts(file, data)
+      write_header(file)
+      write_data(file, data)
     end
   end
 
